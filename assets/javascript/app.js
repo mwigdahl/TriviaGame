9cec9451c8b601f3868fcf00fb47ec4d088adb4f
Matt Wigdahl <matt@leverpulley.com>
2019-09-23 13:39:18 -0600

    working out bugs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,22 +17,22 @@ var gameData = {
 var myTrivia = [
     {
         question: "A river which flows into another river is called a",
-        answers: ["Tributary", "Confluence", "Watershed", "Drainage basin"],
+        choices: ["Tributary", "Confluence", "Watershed", "Drainage basin"],
         correct: "Tributary"
     },
     {
         question: "How many whitewater classification categories are there?",
-        answers: ["Four", "Six", "Two", "Nine"],
+        choices: ["Four", "Six", "Two", "Nine"],
         correct: "Six"
     },
     {
         question: "The place where a river begins is called its:",
-        answers: ["Spring", "Confluence", "Source", "Watershed"],
+        choices: ["Spring", "Confluence", "Source", "Watershed"],
         correct: "Source"
     },
     {
         question: "What year did john wesley powell run the grand canyon?",
-        answers: ["1845", "1869", "1922", "1908"],
+        choices: ["1845", "1869", "1922", "1908"],
         correct: "1869"
     }
 ];
@@ -48,82 +48,102 @@ $('#trivia').on('click', function () {
 
 // load up and display question
 function displayQ() {
-
+ if (gameData.qcounter < 4  && !gameData.endGame) {
     $('#trivia').html(myTrivia[gameData.qcounter].question);
+    $('#answer').empty();
     createBtns();  // load up possible anwsers
     countDown();
-    console.log("timer ", gameData.timer);
+    
+} else if (gameData.qcounter === 4  && !gameData.endGame) {
+    $('#trivia').empty();
+    $('#answer').empty();
+    endGame = true;
+    finalScore();
+} else;
+   
+    //console.log("timer ", gameData.timer);
 
     $('.anwsers').on('click', function () {
+        
+        let selectedAnwser = $(this).attr("data-name");
+        console.log("selectedAnswer", selectedAnwser);
+        console.log("Correct Answer", myTrivia[gameData.qcounter].correct);
 
-        var selectedAnwser = $(this).attr("data-name");
-        console.log("data-name ", selectedAnwser);
-        console.log(myTrivia[gameData.qcounter].correct);
-
-        if (selectedAnwser === myTrivia[gameData.qcounter].correct) {
-            gameData.qcounter++;
-            $('#trivia-answers').empty();
-            $('#trivia').html('<h2>CORRECT!!!</h2>');
+        if ( gameData.timer > 0 && selectedAnwser === myTrivia[gameData.qcounter].correct) {
+            gameData.correct++;
+            $('#trivia-choices').empty();
+            $('#answer').html('<h2>CORRECT!!!</h2>');
+            $('#wins').html('Your Wins: ' + gameData.correct);
+            $('#losses').html('Your Losses: ' + gameData.incorrect);
+            $('#unanswered').html('Unanswered: ' + gameData.unanswered);
             stop();
-            questionResult();
-        } else if (selectedAnwser !== myTrivia[gameData.qcounter].correct) {
+            nextQuestion();
+            
+        } else if (gameData.timer > 0 && selectedAnwser !== myTrivia[gameData.qcounter].correct) {
             gameData.incorrect++;
-            $('#trivia-answers').empty();
-            $('#trivia').html('<h2>WRONG!!!</h2>');
-            questionResult();
+            $('#trivia-choices').empty();
+            $('#answer').html('<h2>WRONG!!!</h2>');
+            $('#wins').html('Your Wins: ' + gameData.correct);
+            $('#losses').html('Your Losses: ' + gameData.incorrect);
+            $('#unanswered').html('Unanswered: ' + gameData.unanswered);
             stop();
-        }
-        if (gameData.qcounter !== 5) {
-            gameData.qcounter++;
-        
-        }
-        
-        if (gameData.qcounter !== 5) {
-            //gameData.qcounter++;
-        } else if (gameData.qcounter === 5 && !gameData.endGame) {
+            nextQuestion();
+        } else if (gameData.timer === 0) {
+            gameData.unanswered++;
+            $('#trivia-choices').empty();
+            $('#trivia').html('<h2>OOPS!!!</h2>');
+            $('#wins').html('Your Wins: ' + gameData.correct);
+            $('#losses').html('Your Losses: ' + gameData.incorrect);
+            $('#unanswered').html('Unanswered: ' + gameData.unanswered);
             stop();
-            endGame = true;
-            finalScore();
+            nextQuestion();
+
         }
     });
 }
 
 
-//Creates button for all possible answers to a question
+
+//Creates button for all possible choices to a question
 
 function createBtns() {
-    for (var i = 0; i < myTrivia.length; i++) {
+    $('#trivia-choices').empty();
+    for (var i = 0; i < myTrivia[gameData.qcounter].choices.length; i++) {
 
-        var myAnswers = $('<button>');
-        myAnswers.attr('data-name', myTrivia[gameData.qcounter].answers[i]);
-        myAnswers.addClass('anwsers');
-        myAnswers.text(myTrivia[gameData.qcounter].answers[i]);
-        $('#trivia-answers').append(myAnswers);
+        var myChoices = $('<button>');
+        myChoices.attr('data-name', myTrivia[gameData.qcounter].choices[i]);
+        myChoices.addClass('anwsers');
+        myChoices.text(myTrivia[gameData.qcounter].choices[i]);
+        $('#trivia-choices').append(myChoices);
 
     }
 
 }
 
 function finalScore() {
-    
+    $('#trivia').html('<h2>Game Over!</h2>');
+    $('#wins').html('Your Wins: ' + gameData.correct);
+    $('#losses').html('Your Losses: ' + gameData.incorrect);
+    $('#unanswered').html('Unanswered: ' + gameData.unanswered);
 }
 
 function nextQuestion() {
+    gameData.qcounter++;
     $('#trivia').html('<button>Next Question</button>');
     $('#trivia').on('click', function () {
         displayQ();
     });
-
+if (gameData.endGame) {
+    finalScore()
+}
 }
 
 function questionResult() {
-    //$('#trivia').empty();
-    $('#trivia-answers').empty();
-    // $('#trivia').html('<h2>Show something.</h2>');
-    $('#wins').html('Your Wins: ', gameData.correct);
-    $('#losses').html('Your Losses: ', gameData.incorrect);
+    //gameData.qcounter++;
+    $('#trivia').empty();
+    $('#trivia-choices').empty();
     reset();
-    nextQuestion();
+    //nextQuestion();
 
 
 }
@@ -144,24 +164,27 @@ function questionResult() {
 // Timer Functions 
 
 function countDown() {
+    $('#countdown').text("Time Remaining: 10");
+    clearInterval(intervalId);
     intervalId = setInterval(decrement, 1000);
 }
 
 function decrement() {
     gameData.timer--;
     $('#countdown').text("Time Remaining: " + gameData.timer);
-
+    
     if (gameData.timer === 0) {
         stop();
         gameData.unanswered++;
         //nextQuestion(); //if timer runs out move to next question
     }
-
+    
 }
 
 function stop() {
     clearInterval(intervalId);
     $('#countdown').empty();
+    //gameData.timer = 10;
 }
 
 
